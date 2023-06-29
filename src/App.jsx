9888955fa4186763c60f8fd05f20a9f71efc9f32
Tanmay <cooltanmayvig@gmail.com>
2023-06-29 22:11:21 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Products from './components/Products'
 import Reviews from './components/Reviews'
 import Footer from './components/Footer'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+
+import {Button} from 'react-bootstrap'
 
 export default function Hero() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Button>Hello</Button>
+    </>
   )
 }
