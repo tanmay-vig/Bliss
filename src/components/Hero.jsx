cd0a463da41b2c12935cef051c8db77ee49eb56f
Tanmay <cooltanmayvig@gmail.com>
2023-07-06 18:49:37 +0530

    khjhk

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-import Carousel from 'react-bootstrap/Carousel';
 import './Hero.css'
+import Carousel from 'react-bootstrap/Carousel';
 import pic1 from '../assets2/asset 1.jpeg'
 import pic2 from '../assets2/asset 2.jpeg'
 import pic4 from '../assets2/asset 4.jpeg'
@@ -12,10 +12,10 @@ export default function Hero() {
         src={pic1}
         alt="First slide"
       />
-      <Carousel.Caption>
+      <div className='carousel-caption1'>
         <h1 className=' text-black'>Get Top-of-the-line IT <br />Products on Rent for <br />Your Business</h1>
         <div className=' text-black d-none d-lg-block d-md-none'>Why invest in IT products? Choose from our wide <br /> range of IT rental plans and grab your pick today.</div>
-      </Carousel.Caption>
+      </div>
     </Carousel.Item>
     <Carousel.Item>
       <img
@@ -24,10 +24,10 @@ export default function Hero() {
         alt="Second slide"
       />
 
-      <Carousel.Caption>
+      <div className='carousel-caption1'>
         <h1 className=' text-black'>Server Rentals for <br /> your IT Needs</h1>
         <div className=' text-black d-none d-lg-block d-md-none'>We offer efficient servers on rent across the country. <br /> Rent from a wide range of blade server, tower server, <br />or rack mount server to fulfill all your IT needs.</div>
-      </Carousel.Caption>
+      </div>
     </Carousel.Item>
     <Carousel.Item>
       <img
@@ -36,12 +36,12 @@ export default function Hero() {
         alt="Third slide"
       />
 
-      <Carousel.Caption>
+      <div className='carousel-caption1'>
         <h1 className=' text-black'>Rent IT Equipment <br />for Your Business Needs</h1>
         <div className=' text-black d-none  d-lg-block d-md-none'>
         Transform your place into a productive work environment <br />by renting all the IT equipment you need from Get It Rent.
         </div>
-      </Carousel.Caption>
+      </div>
     </Carousel.Item>
   </Carousel>
     </>
