bdba52b71f7d6ebebc28724bcafc50a6aac6dc10
Tanmay <cooltanmayvig@gmail.com>
2023-07-09 12:06:02 +0530

    jnj

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,8 +2,9 @@
 
 export default function Clients() {
   return (
-    <>
-      
+    <> <br />
+      <h2 className="text-center">Our Clientele</h2> <br />
+      <div className="text-center">Our mission is to provide superior products at reasonable prices backed by professional support. Here is a list of our esteemed clientele.</div>
     </>
   )
 }
