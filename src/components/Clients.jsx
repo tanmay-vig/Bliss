daff1d8fa69d1823e4fcca129b08479520991803
Tanmay <cooltanmayvig@gmail.com>
2023-07-09 12:18:28 +0530

    jj

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,10 +1,17 @@
 
-
+import pic1 from '../assets2/clients1.jpeg'
+import pic2 from '../assets2/clients2.jpeg'
+import './clients.css'
 export default function Clients() {
   return (
-    <> <br />
+    <section className='clients'> <br />
       <h2 className="text-center">Our Clientele</h2> <br />
-      <div className="text-center">Our mission is to provide superior products at reasonable prices backed by professional support. Here is a list of our esteemed clientele.</div>
-    </>
+      <div className="text-center">Our mission is to provide superior products at reasonable 
+      prices backed by professional support. Here is a list of our esteemed clientele.</div> <br />
+      <div className='client-image'>
+        <img src={pic1} alt="pic1" />
+        <img src={pic2} alt="pic2"  />
+      </div>
+    </section>
   )
 }
