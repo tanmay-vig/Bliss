7cdf530f409a2ca9e8d40bb7ead9da29efb01c1c
Tanmay <cooltanmayvig@gmail.com>
2023-07-11 16:30:45 +0530

    ggg

--- src/pages/products/Laptops.jsx
+++ src/pages/products/Laptops.jsx
@@ -1,12 +1,143 @@
-import './common.css'
-import pic1 from './products/laptop1.jpg'
-import pic2 from './products/laptop2.jpg'
-import pic3 from './products/laptop3.jpg'
-import pic4 from './products/laptop4.jpg'
+import "./common.css";
+import pic1 from "./products/laptop1.jpg";
+import pic2 from "./products/laptop2.jpg";
+import pic3 from "./products/laptop3.jpg";
+import pic4 from "./products/laptop4.jpg";
 export default function laptops() {
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <> <br />
+            <div className="p23">
+                <div className="p11">
+                    <img src={pic1} alt="pic1" /> <br />
+                    <h4>Core i7 Laptop Ram 16GB</h4>
+                    <p>
+                        Intel Core i7 / Ram 16GB / Hard Drive 320GB SATA / Web
+                        Cam / Bluetooth/ Wi-Fi /15.4" Screen Size/Dos
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic2} alt="pic2" /> <br />
+                    <h4>Core i5 Laptop Ram 4GB</h4>
+                    <p>
+                        Intel Core i5 / Ram 4GB/ Hard Drive 320GB/ Web Cam /
+                        Bluetooth/ Wi-Fi /14.1" Screen Size
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic3} alt="pic3" /> <br />
+                    <h4>AMD</h4>
+                    <p>Ryzen 5 8GB DDR4 512 SSD/14"</p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic4} alt="pic4" /> <br />
+                    <h4>Core i5 Laptop Ram 8GB</h4>
+                    <p>
+                        Intel Core i5 / Ram 8GB/ Hard Drive 256GB SSD/ Web Cam /
+                        Bluetooth/ Wi-Fi /14.1" Screen Size
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+            </div>
+            {/* Contents */}
+            <div className="p33">
+                <h2>Get Corporate Laptop Rental Services in Delhi NCR</h2>{" "}
+                <br />
+                Worried about finances to purchase a laptop for your office?
+                Buying laptops is a thing of the past. Renting them, however, is
+                a modern and affordable solution to all your budget-related
+                worries. You can now avail the opportunity to get a corporate
+                laptop rental at Bliss Info Systems.
+                <br />
+                At Get It Rent, we aim to meet our customers' quantitative and
+                qualitative demands. Even if you have large office spaces, you
+                can get Bulk laptop rental from Bliss Infosystems in any model.
+                <br />
+                Some of our laptops include Core i3 Laptop 4th Gen, Core i5
+                Laptop 4th Gen, Core i5 Laptop 5th Gen, and Core i5 Laptop 7th
+                Gen, etc. Find the best laptop for yourself based on the
+                specifications and features listed below
+                <h3>Our Range of Exclusive Laptops on Rent</h3>
+                <br />
+                <b>Core i7 Laptop</b> <br /> <br />
+                With an Intel Core i7 processor and a ram of 16GB, this laptop
+                is perfect for your office requirements. It has sufficient
+                storage with a hard drive of 320GB SATA. It also has excellent
+                battery life and thus, its performance and endurance make it
+                perfect for both work and play, making it the most apt choice
+                for all age groups.
+                <br /> <br />
+                <b>Core i5 Laptop</b> <br /> <br />
+                A Intel Core i5 processor and 4GB RAM makes Core i5 Laptop well
+                suited for work and education requirements. With excellent
+                storage of 320 GB, it is the best laptop that you can buy today.
+                Its competitive features make it the best and the most efficient
+                choice.
+                <br />
+                With just on rent, this deal is perfect for students or for new
+                startups looking for laptops on rent.
+                <br /> <br />
+                <b>AMD</b> <br /> <br />
+                The AMD Ryzen is a compact and reliable laptop. Powered by Ryzen
+                5, it includes 8GB DDR4 512 which makes multitasking easier. It
+                can give one of the best gaming or work experiences. This easy
+                to carry and handle laptop is feature-packed and is as extensive
+                on the inside, as compact it looks on the outside.
+                <br /> <br />
+                <b>Core i5 Laptop</b> <br /> <br />
+                The Intel Core i5 processor and 8GB ram deliver satisfactory
+                performance in all aspects. Along with its speedy performance,
+                it has 256GB SSD internal storage. Equipped with best-in-class
+                features, this laptop delivers impressive performance, catering
+                to all your work demands. <br />
+                <h2>Significance Of a Corporate Laptop Rental</h2>
+                We understand that laptops are the most basic requirement for
+                any company to function smoothly. Finances should never be a
+                hurdle in making your company more productive and efficient.
+                Lack of infrastructure should never be a pushback for a
+                company's growth, which is why renting a laptop is the way
+                forward, especially for startups.
+                <h3>How To Avail Our Rental Services?</h3>
+                Access to laptops is not only affordable but is also easily
+                accessible now. Here's a step-by-step instruction on how our
+                rental process works.
+                <br />
+                <b>Get in touch with us: </b>All you need to do is contact our
+                team and communicate about your laptop requirements, for
+                instance, the storage or processor requirement, camera quality,
+                screen resolution or screen size, etc. Once done, we will pick
+                the most suitable laptop according to your requirements and
+                reserve it for you. <br /> <br />
+                <b>Registration: </b>
+                Our booking and delivery process is speedy, hassle-free, and
+                convenient. You can enjoy the experience of working with
+                different laptop specifications for short and long periods, and
+                that too at the most affordable prices. <br /> <br />
+                <b>Rental agreement: </b>
+                An agreement is signed which mentions the period and the fees of
+                the rental agreement.
+                <br /> <br />
+                <b>Shipping: </b>
+                The laptops we ship are thoroughly examined and checked
+                beforehand by our team.
+                <br /> <br />
+                <b>Feedback call from our customer care: </b>
+                Our company cares about you even after you receive the product.
+                Our customer care will contact you to ensure that our company
+                has given you a product that matches your specifications and
+                suits your requirements.
+                <br />
+                We do not believe in pressuring our customers. You can take a
+                laptop on rent and return it after your requirement is met.
+                <h3>Note To Our Customers</h3>
+                Providing the best services to every customer is always our
+                priority. Therefore, we will highly appreciate it if our laptops
+                are returned undamaged and in the same state as they were handed
+                over to our customers. Your essential support will help us reach
+                more clients and provide them with our exemplary services.
+            </div>
+        </>
+    );
 }
