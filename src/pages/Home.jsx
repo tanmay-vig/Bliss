72db5aa46ece5fcd7707ad26ee3a5407202e2091
Tanmay <cooltanmayvig@gmail.com>
2023-07-11 22:22:30 +0530

    kkk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,15 @@ import Hero from '../components/Hero'
 import Products from '../components/Products'
 import Reviews from '../components/Reviews'
 import Categories from '../components/Categories'
+import './home.css'
 export default function Home() {
   return (
     <>
+    <a href="https://wa.me/9212697071">
+      
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" alt="whatsapp" 
+    className='whatsapp'/>
+    </a>
       <Hero />
       <Categories />
     <Products />
--- src/pages/products/Desktop.jsx
+++ src/pages/products/Desktop.jsx
@@ -1,40 +1,106 @@
-
-import './common.css'
-import pic1 from './products/desktop1.jpg'
-import pic2 from './products/desktop2.jpg'
-import pic3 from './products/desktop3.jpg'
+import "./common.css";
+import pic1 from "./products/desktop1.jpg";
+import pic2 from "./products/desktop2.jpg";
+import pic3 from "./products/desktop3.jpg";
 export default function desktop() {
-  return (
-    <>
-      <div className="p23">
+    return (
+        <>
+            <div className="p23">
                 <div className="p11">
                     <img src={pic1} alt="pic1" /> <br />
-                    <h4>Core i7 Desktop
-</h4>
+                    <h4>Core i7 Desktop</h4>
                     <p>
-                    Intel Core i7/4th Gen/16GB DDR3 RAM /1TB HDD /DVD/ 16″LED/DELL KBD/MOUSE
-
+                        Intel Core i7/4th Gen/16GB DDR3 RAM /1TB HDD /DVD/
+                        16″LED/DELL KBD/MOUSE
                     </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic2} alt="pic2" /> <br />
-                    <h4>Core i3 Desktop
-</h4>
-                    <p>Intel Core i3/8GB RAM/500GB-1TB HDD/LAN/16”LED/DELL KBD/MOUSE
-
+                    <h4>Core i3 Desktop</h4>
+                    <p>
+                        Intel Core i3/8GB RAM/500GB-1TB HDD/LAN/16”LED/DELL
+                        KBD/MOUSE
                     </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic3} alt="pic3" /> <br />
-                    <h4>Core i5 Desktop
-</h4>
-                    <p>Intel Core i5/ 3rd Gen/4GB RAM/500GB HDD/LAN/16”LED/DELL KBD/MOUSE
-</p>
+                    <h4>Core i5 Desktop</h4>
+                    <p>
+                        Intel Core i5/ 3rd Gen/4GB RAM/500GB HDD/LAN/16”LED/DELL
+                        KBD/MOUSE
+                    </p>
                     <a href="">Request Quote</a>
                 </div>
             </div>
-    </>
-  )
+            {/* Contents */}
+            <div className="p33">
+                <h2>One-Stop-Source to Get Desktop on Rent in Delhi NCR</h2>{" "}
+                <br />
+                Desktops are necessary for your company, and renting can help
+                you save money, lessen stress, and boost staff output if your
+                company is expanding quickly. It's not unusual for businesses to
+                rent PCs to meet their needs for office IT equipment. From a
+                financial standpoint, it makes sense for many firms to rent
+                rather than purchase all of their IT equipment.
+                <br />
+                <h3>Advantages of Desktops</h3>
+                <br />
+                <h3>Significance Of a Corporate Laptop Rental</h3>
+                Desktop computers are larger than laptop computers and are
+                frequently hooked into a power outlet. They are a wonderful
+                option if you only need one computer, such as at your home
+                office. The following are some of the benefits of utilizing a
+                desktop computer: <br /> <br />
+                <b>Affordability: </b>Desktop PCs are typically less expensive
+                than laptop computers. <br /> <br />
+                <b>Durability: </b>These computers are frequently constructed
+                from a single piece of strong material, making them extremely
+                robust.
+                <br /> <br />
+                <b>No breakable parts: </b>
+                Desktop computers have no breakable parts. They can be used by
+                children and pets without fear of breaking something.
+                <br /> <br />
+                <b>Good for gaming: </b>Many gaming PCs contain huge,
+                high-quality graphics cards, making them an excellent choice for
+                gamers.
+                <br /> <br />
+                <b>Larger storage: </b>
+                They also have larger hard drives that can hold more data. As a
+                result, desktop computers are ideal for things like gaming,
+                multimedia editing, and CAD (Computer aided design).
+                <br />
+                <h3>Why Choose Bliss Info Systems?</h3>
+                Although almost all businesses now depend on Desktop to run
+                smoothly, this does not change the reality that they are still
+                expensive and difficult to maintain. You don't want to spend
+                much money on expensive machines that will soon become obsolete.
+                Thus opt for a Desktop rental to save money. Here are the
+                reasons why you should choose Bliss Info Systems: <br /> <br />
+                <b>
+                    Bliss Info Systems is a successful company doing everything
+                    possible to assist people.
+                </b>{" "}
+                <br /> <br />
+                We have a team of competent and skilled engineers who meet all
+                of the desktop dental needs of our customers. Our business is
+                committed to satisfying consumer needs, and we vow never to let
+                you down. Any type of desktop computer, including Core i7, Core
+                i3 Desktop, Core i3, Core i5, Desktop Intel Core i5 laptop, and
+                many more, is available for rent. <br /> <br />
+                <b>Bliss InfoSystems is a trusted company</b> <br /> <br />
+                We have a large clientele and have earned our business and
+                received positive feedback. We don't put our customers under any
+                strain during the reservation process or the rental period. Your
+                cooperation will also be very beneficial to our business. You
+                must cooperate with us when making bookings, make payments on
+                schedule and deliver the product back to us damage-free.
+                <br />
+                Contact us anytime to take advantage of our affordable desktop
+                rental services.
+            </div>
+        </>
+    );
 }
