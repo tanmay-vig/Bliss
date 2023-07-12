e819804cdf4fcbae0515c90515811d641f22b2ed
Tanmay <cooltanmayvig@gmail.com>
2023-07-12 11:11:17 +0530

    dd

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,6 +3,11 @@ import pic1 from '../assets2/asset 5.png'
 import pic2 from '../assets2/asset 10.png'
 import pic4 from '../assets2/asset 12.png'
 import pic5 from '../assets2/asset 13.png'
+import pic6 from '../assets2/asset 6.png'
+import pic7 from '../assets2/asset 7.png'
+import pic8 from '../assets2/asset 8.png'
+import pic9 from '../assets2/asset 9.png'
+import pic10 from '../assets2/asset 11.png'
 import './categories.css'
 import {Link} from 'react-router-dom'
 export default function Categories() {
@@ -27,7 +32,33 @@ export default function Categories() {
             <img src={pic5} alt="pic" />
             <div>Desktops</div>
         </Link>
+        <Link to="/cctv" className="r1">
+            <img src={pic6} alt="pic" />
+            <div>CCTV</div>
+        </Link>
+        </div>
+         <br />
+        <div className="c">
+        
+        
+        <Link to="/scanner" className="r1">
+            <img src={pic7} alt="pic" />
+            <div>Scanner</div>
+        </Link>
+        <Link to="/projector" className="r1">
+            <img src={pic8} alt="pic" />
+            <div>Projector</div>
+        </Link>
+        <Link to="/walkie" className="r1">
+            <img src={pic9} alt="pic" />
+            <div>Walkie Talkie</div>
+        </Link>
+        <Link to="/apple" className="r1">
+            <img src={pic10} alt="pic" />
+            <div>Apple</div>
+        </Link>
       </div>
+      
     </section>
   )
 }
