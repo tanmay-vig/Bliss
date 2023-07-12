56cd3df2e5c54607dbbccee01b20364dd6a6472f
Tanmay <cooltanmayvig@gmail.com>
2023-07-12 20:48:08 +0530

    dddd

--- src/pages/products/Apple.jsx
+++ src/pages/products/Apple.jsx
@@ -1,40 +1,121 @@
-import './common.css'
-
+import "./common.css";
+import pic1 from "./products/apple1.jpg";
+import pic2 from "./products/apple2.jpg";
+import pic3 from "./products/apple3.jpg";
+import pic4 from "./products/apple4.jpg";
 export default function Apple() {
-  return (
-    <>
-     
-     <div className="p33">
-      <h2>Apple</h2>
-Chic, Versatile, Compatible- Apple devices are all three. Apple devices can be your game-changer if you're a multi-tasker or an ambitious workaholic. Their super-fast OS makes multitasking smooth, increasing your efficiency.
+    return (
+        <>
+            <br />
+            <h3 className="text-center m-5">
+                Choose from the wide range of Apple products on rent and deck up
+                your team to get the best productivity at the workplace.
+            </h3>
+            <div className="p23">
+                <div className="p11">
+                    <img src={pic1} alt="pic1" /> <br />
+                    <h4> Apple MacBook Pro</h4>
+                    <p>
+                        15-inch Laptop (Core i7/16GB/512GB/Mac OS/Integrated
+                        Graphics)
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic2} alt="pic2" /> <br />
+                    <h4>Apple MacBook Air</h4>
+                    <p>13-inch Laptop (Core i5/8GB/128GB SSD/MacOS)</p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic3} alt="pic3" /> <br />
+                    <h4>Apple i Pad</h4>
+                    <p>
+                        IOS v10, 8MP, 9.7 inch, 32GB, Wi-Fi + 4G LTE + Voice
+                        Calling, 32.4 Wh battery
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+                <div className="p11">
+                    <img src={pic4} alt="pic4" /> <br />
+                    <h4>Apple iPad Pro
+</h4>
+                    <p>Liquid Retina display 11-inch/ Wi-Fi/ 256GB
 
-Regarding high-end security, durability, and trustworthiness, Apple MacBook and iPad stand unparalleled against all other devices available in the market. Its impeccable design, battery life, and smooth and fast functioning make it the go-to choice for anyone looking for a compatible device for office or school use.
-
-Apple For Work
-Apple products are renowned for their flawless performance and phenomenal display. World-class professional software like Final Cut Pro, Adobe Premiere, InDesign, Photoshop, etc., can work smoothly without crashes or hangs on Apple products.
-
-Seamless Functioning
-Apple products are advanced enough for the work requirements of the 21st century. Doing one thing at a time is a thing of the past. With Apple products, you can skim through various tabs and work between various applications simultaneously.
-
-Data Protection And Storage
-These devices can sync flawlessly to all devices and have a secure back option in iCloud for all your data. This data is not only saved but is also protected with one of the most high-end security systems.
-
-Longer Battery Life
-A good battery backup is a prerequisite for being productive and efficient at any time of the day. MacBooks and iPad can work long hours because of their good battery life.
-
-Video editing
-Also, its comparatively high-quality camera with amazing photography and videography tools, paired with a massive display, adds to the endless list of things possible with its usage.
-
-Apple Products For Students And Artists
-MacBooks and iPads are lightweight and are hence portable and user-friendly. iPads can satisfy your creativity quotient, especially if you are a student or art professional. You can work with various apps and create digital notes, scribble designs, and artwork through the Apple pencil.
-
-Moreover, if you're a podcast enthusiast, you can listen to and record your podcast and music. Youtube blogs, Instagram reels, and social media management can be much easier through the latest video editing apps. With education being digitized, Apple products can also give children access to many new learning aids, applications, and tools, giving them an all-around learning experience.
-
-While appearances can be deceptive, the same does not stand true for apple products. They have an edgy, sleek, and classy look, which is an exact replication of their seamless functioning and powerful software.
-
-Get Apple Products on Get It Rent
-You can experience the working of a MacBook or iPad and get first-hand experience of its exceptional design and working. And you needn't have a big budget to experience these high-end facilities. You can purchase apple products on rent and make your work truly 'hassle free'.
-      </div> 
-    </>
-  )
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+            </div>
+            <div className="p33">
+                <h2>Apple</h2>
+                Chic, Versatile, Compatible- Apple devices are all three. Apple
+                devices can be your game-changer if you're a multi-tasker or an
+                ambitious workaholic. Their super-fast OS makes multitasking
+                smooth, increasing your efficiency.
+                <br /> <br />
+                Regarding high-end security, durability, and trustworthiness,
+                Apple MacBook and iPad stand unparalleled against all other
+                devices available in the market. Its impeccable design, battery
+                life, and smooth and fast functioning make it the go-to choice
+                for anyone looking for a compatible device for office or school
+                use.
+                <br /> <br />
+                <b>Apple For Work</b> <br /> <br />
+                Apple products are renowned for their flawless performance and
+                phenomenal display. World-class professional software like Final
+                Cut Pro, Adobe Premiere, InDesign, Photoshop, etc., can work
+                smoothly without crashes or hangs on Apple products.
+                <br /> <br />
+                <b>Seamless Functioning</b> <br /> <br />
+                Apple products are advanced enough for the work requirements of
+                the 21st century. Doing one thing at a time is a thing of the
+                past. With Apple products, you can skim through various tabs and
+                work between various applications simultaneously.
+                <br /> <br />
+                <b>Data Protection And Storage</b> <br /> <br />
+                These devices can sync flawlessly to all devices and have a
+                secure back option in iCloud for all your data. This data is not
+                only saved but is also protected with one of the most high-end
+                security systems.
+                <br /> <br />
+                <b>Longer Battery Life</b> <br /> <br />
+                A good battery backup is a prerequisite for being productive and
+                efficient at any time of the day. MacBooks and iPad can work
+                long hours because of their good battery life.
+                <br /> <br />
+                <b>Video editing</b> <br /> <br />
+                Also, its comparatively high-quality camera with amazing
+                photography and videography tools, paired with a massive
+                display, adds to the endless list of things possible with its
+                usage.
+                <br /> <br />
+                <b>Apple Products For Students And Artists</b> <br /> <br />
+                MacBooks and iPads are lightweight and are hence portable and
+                user-friendly. iPads can satisfy your creativity quotient,
+                especially if you are a student or art professional. You can
+                work with various apps and create digital notes, scribble
+                designs, and artwork through the Apple pencil.
+                <br /> <br />
+                Moreover, if you're a podcast enthusiast, you can listen to and
+                record your podcast and music. Youtube blogs, Instagram reels,
+                and social media management can be much easier through the
+                latest video editing apps. With education being digitized, Apple
+                products can also give children access to many new learning
+                aids, applications, and tools, giving them an all-around
+                learning experience.
+                <br /> <br />
+                While appearances can be deceptive, the same does not stand true
+                for apple products. They have an edgy, sleek, and classy look,
+                which is an exact replication of their seamless functioning and
+                powerful software.
+                <br /> <br />
+                <h3>Get Apple Products on Bliss Info Systems</h3> <br />
+                You can experience the working of a MacBook or iPad and get
+                first-hand experience of its exceptional design and working. And
+                you needn't have a big budget to experience these high-end
+                facilities. You can purchase apple products on rent and make
+                your work truly 'hassle free'.
+            </div>
+        </>
+    );
 }
