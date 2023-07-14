ee0cee38bb030ca13a6ec454473786954baaf938
Tanmay <cooltanmayvig@gmail.com>
2023-07-14 14:28:15 +0530

    n

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,6 +1,41 @@
 
-import pic1 from '../assets2/clients1.jpeg'
-import pic2 from '../assets2/clients2.jpeg'
+// import pic1 from '../assets2/clients1.jpeg'
+// import pic2 from '../assets2/clients2.jpeg'
+import pic1 from '../cus/aa.png'
+import pic2 from '../cus/au.png'
+import pic3 from '../cus/cr.png'
+import pic4 from '../cus/dt.png'
+import pic5 from '../cus/ess.png'
+import pic6 from '../cus/gb.png'
+import pic7 from '../cus/ho.png'
+import pic8 from '../cus/ht.png'
+import pic9 from '../cus/ip.png'
+import pic10 from '../cus/kr.png'
+import pic11 from '../cus/mu.png'
+import pic12 from '../cus/nu.png'
+import pic13 from '../cus/rh.png'
+import pic14 from '../cus/rm.png'
+import pic15 from '../cus/sp.png'
+import pic16 from '../cus/t1.png'
+import pic17 from '../cus/ub.png'
+import pic18 from '../cus/v.png'
+import pic19 from '../vend/2c.png'
+import pic20 from '../vend/ad.png'
+import pic21 from '../vend/aw.png'
+import pic22 from '../vend/az.png'
+import pic23 from '../vend/be.png'
+import pic24 from '../vend/ci.png'
+import pic25 from '../vend/co.png'
+import pic26 from '../vend/de.png'
+import pic27 from '../vend/hc.png'
+import pic28 from '../vend/hp.png'
+import pic29 from '../vend/in.png'
+import pic30 from '../vend/l.png'
+import pic31 from '../vend/lg.png'
+import pic32 from '../vend/ms.png'
+import pic33 from '../vend/so.png'
+import pic34 from '../vend/ss.png'
+import pic35 from '../vend/wd.png'
 import './clients.css'
 export default function Clients() {
   return (
@@ -11,6 +46,39 @@ export default function Clients() {
       <div className='client-image'>
         <img src={pic1} alt="pic1" />
         <img src={pic2} alt="pic2"  />
+        <img src={pic3} alt="pic2"  />
+        <img src={pic4} alt="pic2"  />
+        <img src={pic5} alt="pic2"  />
+        <img src={pic6} alt="pic2"  />
+        <img src={pic7} alt="pic2"  />
+        <img src={pic8} alt="pic2"  />
+        <img src={pic9} alt="pic2"  />
+        <img src={pic10} alt="pic2"  />
+        <img src={pic11} alt="pic2"  />
+        <img src={pic12} alt="pic2"  />
+        <img src={pic13} alt="pic2"  />
+        <img src={pic14} alt="pic2"  />
+        <img src={pic15} alt="pic2"  />
+        <img src={pic16} alt="pic2"  />
+        <img src={pic17} alt="pic2"  />
+        <img src={pic18} alt="pic2"  />
+        <img src={pic19} alt="pic2"  />
+        <img src={pic20} alt="pic2"  />
+        <img src={pic21} alt="pic2"  />
+        <img src={pic22} alt="pic2"  />
+        <img src={pic23} alt="pic2"  />
+        <img src={pic24} alt="pic2"  />
+        <img src={pic25} alt="pic2"  />
+        <img src={pic26} alt="pic2"  />
+        <img src={pic27} alt="pic2"  />
+        <img src={pic28} alt="pic2"  />
+        <img src={pic29} alt="pic2"  />
+        <img src={pic30} alt="pic2"  />
+        <img src={pic31} alt="pic2"  />
+        <img src={pic32} alt="pic2"  />
+        <img src={pic33} alt="pic2"  />
+        <img src={pic34} alt="pic2"  />
+        <img src={pic35} alt="pic2"  />
       </div>
     </section>
   )
