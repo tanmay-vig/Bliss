be7c0c1b308ceb76dfc93c4684b14637c6583bb5
Tanmay <cooltanmayvig@gmail.com>
2023-07-17 23:20:26 +0530

    ddd

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,85 +1,145 @@
+import Carousel from "react-bootstrap/Carousel";
 
-// import pic1 from '../assets2/clients1.jpeg'
-// import pic2 from '../assets2/clients2.jpeg'
-import pic1 from '../cus/aa.png'
-import pic2 from '../cus/au.png'
-import pic3 from '../cus/cr.png'
-import pic4 from '../cus/dt.png'
-import pic5 from '../cus/ess.png'
-import pic6 from '../cus/gb.png'
-import pic7 from '../cus/ho.png'
-import pic8 from '../cus/ht.png'
-import pic9 from '../cus/ip.png'
-import pic10 from '../cus/kr.png'
-import pic11 from '../cus/mu.png'
-import pic12 from '../cus/nu.png'
-import pic13 from '../cus/rh.png'
-import pic14 from '../cus/rm.png'
-import pic15 from '../cus/sp.png'
-import pic16 from '../cus/t1.png'
-import pic17 from '../cus/ub.png'
-import pic18 from '../cus/v.png'
-import pic19 from '../vend/2c.png'
-import pic20 from '../vend/ad.png'
-import pic21 from '../vend/aw.png'
-import pic22 from '../vend/az.png'
-import pic23 from '../vend/be.png'
-import pic24 from '../vend/ci.png'
-import pic25 from '../vend/co.png'
-import pic26 from '../vend/de.png'
-import pic27 from '../vend/hc.png'
-import pic28 from '../vend/hp.png'
-import pic29 from '../vend/in.png'
-import pic30 from '../vend/l.png'
-import pic31 from '../vend/lg.png'
-import pic32 from '../vend/ms.png'
-import pic33 from '../vend/so.png'
-import pic34 from '../vend/ss.png'
-import pic35 from '../vend/wd.png'
-import './clients.css'
+import pic1 from "../Customer/c1.png";
+import pic2 from "../Customer/c2.png";
+import pic3 from "../Customer/c3.png";
+import pic4 from "../Customer/c4.png";
+import pic5 from "../Customer/c5.png";
+import pic6 from "../Customer/c6.png";
+import pic7 from "../Customer/c7.png";
+import pic8 from "../Customer/c8.png";
+import pic9 from "../Customer/c9.png";
+import pic10 from "../Customer/c10.png";
+import pic11 from "../Customer/c11.png";
+import pic12 from "../Customer/c12.png";
+import pic13 from "../Customer/c13.png";
+import pic14 from "../Customer/c14.png";
+import pic15 from "../Customer/c15.png";
+import pic16 from "../Customer/c16.png";
+import pic17 from "../Customer/c17.png";
+import pic18 from "../Customer/c18.png";
+import pic19 from "../Vendor/v1.png";
+import pic20 from "../Vendor/v2.png";
+import pic21 from "../Vendor/v3.png";
+import pic22 from "../Vendor/v4.png";
+import pic23 from "../Vendor/v5.png";
+import pic24 from "../Vendor/v6.png";
+import pic25 from "../Vendor/v7.png";
+import pic26 from "../Vendor/v8.png";
+import pic27 from "../Vendor/v9.png";
+import pic28 from "../Vendor/v10.png";
+import pic29 from "../Vendor/v11.png";
+import pic30 from "../Vendor/v12.png";
+import pic31 from "../Vendor/v13.png";
+import pic32 from "../Vendor/v14.png";
+import pic33 from "../Vendor/v15.png";
+import pic34 from "../Vendor/v16.png";
+import pic35 from "../Vendor/v17.png";
+import "./clients.css";
 export default function Clients() {
-  return (
-    <section className='clients'> <br />
-      <h2 className="text-center">Our Clientele</h2> <br />
-      <div className="text-center rty3">Our mission is to provide superior products at reasonable 
-      prices backed by professional support. Here is a list of our esteemed clientele.</div> <br />
-      <div className='client-image'>
-        <img src={pic1} alt="pic1" />
-        <img src={pic2} alt="pic2"  />
-        <img src={pic3} alt="pic2"  />
-        <img src={pic4} alt="pic2"  />
-        <img src={pic5} alt="pic2"  />
-        <img src={pic6} alt="pic2"  />
-        <img src={pic7} alt="pic2"  />
-        <img src={pic8} alt="pic2"  />
-        <img src={pic9} alt="pic2"  />
-        <img src={pic10} alt="pic2"  />
-        <img src={pic11} alt="pic2"  />
-        <img src={pic12} alt="pic2"  />
-        <img src={pic13} alt="pic2"  />
-        <img src={pic14} alt="pic2"  />
-        <img src={pic15} alt="pic2"  />
-        <img src={pic16} alt="pic2"  />
-        <img src={pic17} alt="pic2"  />
-        <img src={pic18} alt="pic2"  />
-        <img src={pic19} alt="pic2"  />
-        <img src={pic20} alt="pic2"  />
-        <img src={pic21} alt="pic2"  />
-        <img src={pic22} alt="pic2"  />
-        <img src={pic23} alt="pic2"  />
-        <img src={pic24} alt="pic2"  />
-        <img src={pic25} alt="pic2"  />
-        <img src={pic26} alt="pic2"  />
-        <img src={pic27} alt="pic2"  />
-        <img src={pic28} alt="pic2"  />
-        <img src={pic29} alt="pic2"  />
-        <img src={pic30} alt="pic2"  />
-        <img src={pic31} alt="pic2"  />
-        <img src={pic32} alt="pic2"  />
-        <img src={pic33} alt="pic2"  />
-        <img src={pic34} alt="pic2"  />
-        <img src={pic35} alt="pic2"  />
-      </div>
-    </section>
-  )
+    return (
+        <section className="clients">
+            {" "}
+            <br />
+            <h2 className="text-center">Our Clientele</h2> <br />
+            <div className="text-center rty3">
+                Our mission is to provide superior products at reasonable prices
+                backed by professional support. Here is a list of our esteemed
+                clientele.
+            </div>{" "}
+            <br />
+            <Carousel variant="dark">
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic1} alt="pic2" />
+                        <img src={pic2} alt="pic2" />
+                        <img src={pic3} alt="pic2" />
+                    </div> <br /> <br /> <br /> 
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic4} alt="pic2" />
+                        <img src={pic5} alt="pic2" />
+                        <img src={pic6} alt="pic2" />
+                    </div> <br /> <br />  <br /> 
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic7} alt="pic2" />
+                        <img src={pic8} alt="pic2" />
+                        <img src={pic9} alt="pic2" />
+                    </div> <br /> <br /> <br /> 
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic10} alt="pic2" />
+                        <img src={pic11} alt="pic2" />
+                        <img src={pic12} alt="pic2" />
+                    </div> <br /> <br /> <br /> 
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic13} alt="pic2" />
+                        <img src={pic14} alt="pic2" />
+                        <img src={pic15} alt="pic2" />
+                    </div> <br /> <br /> <br /> 
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic16} alt="pic2" />
+                        <img src={pic17} alt="pic2" />
+                        <img src={pic18} alt="pic2" />
+                    </div> <br /> <br /> <br /> 
+                </Carousel.Item>
+            </Carousel>
+            <br /> <br />
+            {/* Vendors */}
+            <h2 className="text-center">Our Vendors</h2> <br />
+            <div className="text-center rty3">Our trusted vendors</div> <br />
+            <Carousel variant="dark">
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic19} alt="pic2" />
+                        <img src={pic20} alt="pic2" />
+                        <img src={pic21} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic22} alt="pic2" />
+                        <img src={pic23} alt="pic2" />
+                        <img src={pic24} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic25} alt="pic2" />
+                        <img src={pic26} alt="pic2" />
+                        <img src={pic27} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic28} alt="pic2" />
+                        <img src={pic29} alt="pic2" />
+                        <img src={pic30} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic31} alt="pic2" />
+                        <img src={pic32} alt="pic2" />
+                        <img src={pic33} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+                <Carousel.Item> <br />
+                    <div className="client-image">
+                        <img src={pic34} alt="pic2" />
+                        <img src={pic35} alt="pic2" />
+                    </div> <br /> <br />
+                </Carousel.Item>
+            </Carousel>
+            <div className="client-image"></div>
+        </section>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Highlights from '../components/Highlights'
 import Clients from '../components/Clients'
 import Hero from '../components/Hero'
 import Products from '../components/Products'
-import Reviews from '../components/Reviews'
+
 import Categories from '../components/Categories'
 import './home.css'
 export default function Home() {
@@ -22,7 +22,7 @@ export default function Home() {
     <Rent />
     <Highlights />
     <Clients />
-    <Reviews />
+    
     </>
   )
 }
