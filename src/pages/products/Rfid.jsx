81e9e8214cf8a00859deddc2faac34334eeac4af
Tanmay <cooltanmayvig@gmail.com>
2023-07-18 11:07:37 +0530

    ccc

--- src/pages/products/Rfid.jsx
+++ src/pages/products/Rfid.jsx
@@ -16,140 +16,150 @@ export default function Rfid() {
             <h3 className="text-center m-5">
                 Rfid Sensors That satisfies all the needs of the firm
             </h3>
-            
             <div className="p23">
                 <div className="p11">
                     <img src={pic1} alt="pic1" /> <br />
-                    <h4> Baofeng UV 5R Dual band</h4>
+                    <h4> Rfid Printer</h4>
                     <p>
-                        Handheld/Digital/ 400-520MHz, 136-174MHz/ 1.5KM-3KM/
-                        1800mAh/0.46Kg/5W
+                        Can be integrated with existing software <br /> Uses Wax
+                        resin ribbon for bright printing <br /> Capable of
+                        printing more than 10000 tags a day
                     </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic2} alt="pic2" /> <br />
-                    <h4>Motorola MS350R 35 mile</h4>
-                    <p>Handheld/22 CH, 8 repeaters/ 35mile/3 AA/0.22Kg/5W</p>
+                    <h4>
+                        <b>Rfid Jewellery Tags</b>
+                    </h4>
+                    <p>
+                        Reusable Tags and One-time use tags <br />
+                        No Sight for reading the tags <br />
+                        Support international standards and protocols
+                    </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic3} alt="pic3" /> <br />
-                    <h4>Retevis H-777 2-Way</h4>
-                    <p>Handheld/ 400-470MHz/3KM-5KM/16 CH/1000mAh/0.36Kg/5W</p>
-                    <a href="">Request Quote</a>
-                </div>
-            </div>
-
-            <div className="p23">
-                <div className="p11">
-                    <img src={pic4} alt="pic1" /> <br />
-                    <h4> Baofeng UV 5R Dual band</h4>
+                    <h4>
+                        <b>Rfid Handheld Devices</b>
+                    </h4>
                     <p>
-                        Handheld/Digital/ 400-520MHz, 136-174MHz/ 1.5KM-3KM/
-                        1800mAh/0.46Kg/5W
+                        Faster inventory <br />
+                        Alerts during inventory <br />
+                        Quick Search for missing items <br />
+                        Integrated or Standalone system <br />
+                        Easy to carry <br />
+                        Longer battery life <br />
+                        Inbuilt Camera, Sim slot , external display
                     </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
+                    <img src={pic4} alt="pic1" /> <br />
+                    <h4>
+                        {" "}
+                        <b> Smart Rfid Tray </b>
+                    </h4>
+                    <p>
+                        Data Analytics <br />
+                        Provides estimation of one or more products at once{" "}
+                        <br />
+                        Integration with any system
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+            </div>
+            {/* Advanced Rfid */}
+            <br />
+            <h2 className="text-center">Advanced Rfid Products:</h2>
+            <div className="p23">
+                <div className="p11">
                     <img src={pic5} alt="pic2" /> <br />
-                    <h4>Motorola MS350R 35 mile</h4>
-                    <p>Handheld/22 CH, 8 repeaters/ 35mile/3 AA/0.22Kg/5W</p>
+                    <h4><b>RFID Inventory Tray</b></h4>
+                    <p>
+                    Stock Control Management <br />
+ Scans all the items in the tray at once  <br />
+when kept on top of it <br />
+ No Sight for reading the tags <br />
+ Fastens the inventory checking <br />
+ 100 % accurate and safe <br />
+ Focused reading on particular items
+
+                    </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic6} alt="pic3" /> <br />
-                    <h4>Retevis H-777 2-Way</h4>
-                    <p>Handheld/ 400-470MHz/3KM-5KM/16 CH/1000mAh/0.36Kg/5W</p>
-                    <a href="">Request Quote</a>
-                </div>
-            </div>
-
-
-            <div className="p23">
-                <div className="p11">
-                    <img src={pic7} alt="pic1" /> <br />
-                    <h4> Baofeng UV 5R Dual band</h4>
+                    <h4><b>
+                    Jewel Tray Tracking System</b></h4>
                     <p>
-                        Handheld/Digital/ 400-520MHz, 136-174MHz/ 1.5KM-3KM/
-                        1800mAh/0.46Kg/5W
+                    Tray Wise Inventory Management <br />
+ Provides Report at end of the day <br />
+ Alerts if any tray not entering the safe <br />
+ Quick Search for missing items <br />
+ Integrated or Standalone system <br />
+ Easy to carry <br />
+ Monitors the staff who handles 
+the tray
+
                     </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
+                    <img src={pic7} alt="pic1" /> <br />
+                    <h4> <b>
+                    AM + RFID Gate System</b></h4>
+                    <p>
+                    Security Management <br />
+ Staff live tracking <br />
+ Integrated with CCTV <br />
+ Can be installed with any 
+existing EAS Gate
+
+                    </p>
+                    <a href="">Request Quote</a>
+                </div>
+            </div>
+            <div className="p23">
+                <div className="p11">
                     <img src={pic8} alt="pic2" /> <br />
-                    <h4>Motorola MS350R 35 mile</h4>
-                    <p>Handheld/22 CH, 8 repeaters/ 35mile/3 AA/0.22Kg/5W</p>
+                    <h4><b>
+                    2CQR Intelligent Weighing 
+with Inventory Tray</b></h4>
+                    <p>
+                    2CQR Integrated with top most jewel 
+weighing scale “Mettler Toledo” <br />
+ RFID and Weighing scale are integrated to 
+provide the accurate inventory with weight of 
+the jewel. <b></b>
+ Compares the Current weight with actual 
+jewel weight and provides 100% accurate results 
+immediately <br />
+ Simple and Easy Interface <br />
+ Detailed report of each and every item  <br />
+ 
+
+                    </p>
                     <a href="">Request Quote</a>
                 </div>
                 <div className="p11">
                     <img src={pic9} alt="pic3" /> <br />
-                    <h4>Retevis H-777 2-Way</h4>
-                    <p>Handheld/ 400-470MHz/3KM-5KM/16 CH/1000mAh/0.36Kg/5W</p>
+                    <h4>
+                        <b>
+                        Attendance Management:
+Biometric / RFID Card
+                        </b>
+                    </h4>
+                    <p>
+                    Live Tracking of Employees
+ Detailed Reports <br />
+ Day Report / Monthly Report <br />
+ Branch Wise Statistics
+
+                    </p>
                     <a href="">Request Quote</a>
                 </div>
-            </div>
-            {/* Contents */} <br />
-            <div className="p33">
-                <h2>Walkie Talkie</h2>
-                The Walkie-Talkie is a high-quality, long-range connecting
-                device. It can connect you and your friends up to 1 mile away!
-                The Walkie-Talkie has up to 99 channels to choose from, so you
-                can easily stay in touch with your friends and family. With this
-                walkie-talkie, you no longer have to worry about battery life—it
-                has an 8-12 hour battery life to stay connected all day long!
-                <br /> <br />
-                The Walkie-Talkie has a radio frequency of 400-512 MHz and works
-                with up to 99 channels, so you'll never have trouble staying in
-                touch with your group or family. This walkie-talkie also has a
-                frequency of operation at around 400-512 MHz, which means it
-                will work anywhere in the world. <br /> <br />
-                <b>Specialities</b> <br /> <br />
-                The walkie-talkie is the perfect companion for any outdoor
-                adventure. It's waterproof and dustproof so that you can take it
-                with you anywhere—the beach, camping trips, or hiking through
-                the woods. When used with a clear line of sight, it has a range
-                of one mile, so there's no need to worry about your connection
-                getting cut off by trees or other obstacles. And the noise
-                reduction feature means that even if you're in the middle of
-                nowhere and surrounded by birds chirping or waves crashing on
-                the shore, you'll still be able to hear each other.
-                <br /> <br />
-                <b>Design</b> <br /> <br />
-                But what makes this walkie-talkie truly special is its design:
-                it's lightweight and compact enough that you can fit it in your
-                pocket or clip it onto your backpack without weighing yourself
-                down. Plus, its sleek black color looks great with any outfit!
-                <br /> <br />
-                With Bluetooth connectivity, you can connect your walkie-talkie
-                to your smartphone or tablet and use it as a speakerphone.
-                <br /> <br />
-                <b>Emergency Alerter</b> <br /> <br />
-                If you're out on the trail alone or away from civilization for
-                any reason, an emergency alert system is essential for staying
-                safe. It will allow you to send out an SOS signal that can be
-                picked up by anyone who has a compatible device nearby—from
-                other hikers to rescue crews—and provide them with information
-                about where you are so they can reach out and help.
-                <br /> <br />
-                Of course, these features would only be useful if they were
-                waterproof! We've designed our products with IP67 certification
-                to withstand extreme conditions like rain, snow, and submersion.
-                <br /> <br />
-                <b>Key Functionalities</b> <br /> <br />
-                The Walkie Talkie uses the FRS and GMRS frequencies. It has a
-                channel capacity of 16, and the typical RF output is 1 W when
-                operating in low power mode, and 4 W when operating in high
-                power mode. The frequency range of A Walkie Talkie is 400-520
-                MHz. It has a power supply of 5W (Nominal) ± 20%, and it runs on
-                a high capacity Li-ion battery that can provide up to 14.5 hours
-                of operation on analog channels or 18.5 hours of operation on
-                digital channels, depending on the type of channels you are
-                using.
-                <br /> <br />
-                Once fully charged, A Walkie Talkie has a range of 1.5KM-3 KM
-                depending on how many obstructions there are between your unit
-                and the other party's unit.
             </div>
         </>
     );
